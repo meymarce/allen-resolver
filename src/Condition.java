ab9a6f62980bf02b1c0d64a4bb9b920dcb455768
Marcel Meyer <marcel.meyer2@hp.com>
2014-05-06 15:34:04 +0200

    add Condition implementation

--- src/Condition.java
+++ src/Condition.java
@@ -1,4 +1,21 @@
 
-public class Condition {
 
+public class Condition {
+	private char from, to;
+	private String edge;
+	
+	public char getFrom() {
+		return this.from;
+	}
+	
+	public char getTo() {
+		return this.to;
+	}
+	
+	public String getEdge() {
+		return this.edge;
+	}
+	
+	
+	
 }
