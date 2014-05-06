f0c2850aabc1f903dcb0e5287408a5177581c675
Sebastian Woitzik <it11019@lehre.dhbw-stuttgart.de>
2014-05-06 12:53:52 +0200

    Basic structure + basic files

---
+++ src/Output.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+
+public class Output {
+
+	public Output() {
+		
+	}
+	
+	public void printResolution(Condition conditionResolved) {
+		
+	}
+	
+	public void printFinalResolution(List<Condition> conditions, boolean integrity) {
+		
+	}
+}
---
+++ src/Parser.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+
+public class Parser {
+	private List<Condition> conditions;
+	private String bridge;
+	private Resolver resolver;
+	
+	public Parser() {
+		
+	}
+	
+	public void readConditions(String input) {
+		
+	}
+	
+	public void readBridge(String bridge) {
+		
+	}
+}
