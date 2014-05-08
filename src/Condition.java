c694d1ff2f49ac857deeea4ff5823c8de9a1a7e2
Marcel Meyer <marcel.meyer2@hp.com>
2014-05-08 15:48:01 +0200

    JavaDoc, make Resolver use UserInterface

--- src/Condition.java
+++ src/Condition.java
@@ -1,39 +1,82 @@
 
-
+/**
+ * This class represents a condition in the net
+ * 
+ * @author marcel.meyer2@hp.com
+ *
+ */
 public class Condition {
 	private int id;
 	private Character from, to;
 	private String edge;
 	
-	public Condition(int id, char from, char to, String edge) {
+	/**
+	 * Constructor of a condition
+	 * 
+	 * @param id A unique integer to identify the condition (uniqueness is not checked by the constructor)
+	 * @param from The event the condition starts from
+	 * @param to The event the condition ends at
+	 * @param edge The relations between the two events
+	 */
+	public Condition(int id, Character from, Character to, String edge) {
 		this.id = id;
 		this.from = from;
 		this.to = to;
 		this.edge = edge;
 	}	
 	
+	/**
+	 * Method to get the id of the condition
+	 * 
+	 * @return The conditions id
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
-	public char getFrom() {
+	/**
+	 * Method to get the starting event
+	 * 
+	 * @return A character representing the event
+	 */
+	public Character getFrom() {
 		return this.from;
 	}
 	
-	public char getTo() {
+	/**
+	 * Method to get the finishing event
+	 * 
+	 * @return A character representing the event
+	 */
+	public Character getTo() {
 		return this.to;
 	}
 	
+	/**
+	 * Method to get the relations of the condition
+	 * 
+	 * @return A string representing the relations (each relation is comma-separated)
+	 */
 	public String getEdge() {
 		return this.edge;
 	}
 	
-	public void setEdge(String value) {
-		this.edge = value;
-	}
-
+	/**
+	 * Method to get the inverse relations of the condition
+	 * 
+	 * @return A string representing the inverse relations (each relation is comma-separated)
+	 */
 	public String getInverseEdge() {
 		return AllenTable.inverse(this.edge);
+	}
+	
+	/**
+	 * Method to set the relations of the condition
+	 * 
+	 * @param value A string representing the relations to be set (each relation is comma-separated)
+	 */
+	public void setEdge(String value) {
+		this.edge = value;
 	}
 	
 	@Override
--- src/Resolver.java
+++ src/Resolver.java
@@ -2,46 +2,80 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
+/**
+ * Resolver is a class for providing functions proof the completeness of a net and therefore it is able to proof the consistency of single conditions
+ * 
+ * @author marcel.meyer2@hp.com
+ *
+ */
 public class Resolver {
-
-	//private Output output;
+	private UserInterface ui;
+	private List<Condition> conditions;
 	
-	public static void resolveConditions(List<Condition> conditions) {
+	/**
+	 * Constructor of the Resolver
+	 * 
+	 * @param ui The {link UserInterface} that is going to be used
+	 * @param conditions The list of conditions needed for the resolution and the proofing
+	 */
+	public Resolver(UserInterface ui, List<Condition> conditions) {
+		this.ui = ui;
+		this.conditions = conditions;
+	}
+	
+	/**
+	 * This function tries to resolve all possible variations and prints the output using the {@link UserInterface}
+	 *  
+	 */
+	public void resolveConditions() {
 		boolean completeness = true;
 		Condition condition1 = null, condition2 = null, toProof;
+		
+		// mark the toProof condition and the two to use for proofing
 		for( int i = 0; i < 3; ++i) {
 			switch(i) {
 			case 0:
-				condition1 = conditions.get(1);
-				condition2 = conditions.get(2);
+				condition1 = this.conditions.get(1);
+				condition2 = this.conditions.get(2);
 				break;
 			case 1:
-				condition1 = conditions.get(0);
-				condition2 = conditions.get(2);
+				condition1 = this.conditions.get(0);
+				condition2 = this.conditions.get(2);
 				break;
 			case 2:
-				condition1 = conditions.get(0);
-				condition2 = conditions.get(1);
+				condition1 = this.conditions.get(0);
+				condition2 = this.conditions.get(1);
 				break;
 			}
-			toProof = conditions.get(i);
+			toProof = this.conditions.get(i);
 			
+			// resolve and proof the condition and also its reverse
 			completeness = resolveCondition(condition1, condition2, toProof) && completeness;
 			completeness = resolveCondition(condition1, condition2, new Condition(5, toProof.getTo(), toProof.getFrom(), toProof.getInverseEdge())) && completeness;
 		}
 		
+		// output the completeness using the UserInterface
 		if( !completeness ) {
-			System.out.println("Das Netz ist nicht vollständig");
+			ui.showErrorMessage("Das Netz ist nicht vollständig");
 		} else {
-			System.out.println("Das Netz ist vollständig");
+			ui.showStatusMessage("Das Netz ist vollständig");
 		}
 	}
 	
-	private static boolean resolveCondition(Condition condition1, Condition condition2, Condition toProof) {		
+	/**
+	 * This function tries to proof a condition and returns a boolean of success. It also prints the output of success using the {@link UserInterface}
+	 * 
+	 * @param condition1 The first condition to use for the proof (conditions are not sequence-faithful)
+	 * @param condition2 The second condition to use for the proof (conditions are not sequence-faithful)
+	 * @param toProof The condition to proof. This condition has to be in the order to be proven.
+	 * @return True if {@link toProof} could be proven and false if not
+	 */
+	
+	private boolean resolveCondition(Condition condition1, Condition condition2, Condition toProof) {		
 		Condition step1 = null, step2 = null, used = null, unused = null;
 		List<String> resolvedallen, toproofallen, resultallen = new ArrayList<String>();
 		
+		// Determine path to follow for proof
 		if( toProof.getFrom() == condition1.getFrom() ) {
 			used=step1=condition1;			
 		}
@@ -67,6 +101,7 @@ public class Resolver {
 			step2 = new Condition(4, unused.getTo(), unused.getFrom(), unused.getInverseEdge());
 		}
 		
+		// proof by using the pfunction and creating the intersection
 		resolvedallen = Arrays.asList(AllenTable.pfunction(step1.getEdge(), step2.getEdge()).split(","));
 		toproofallen = Arrays.asList(toProof.getEdge().split(","));
 		for( int i = 0; i < resolvedallen.size(); ++i ) {
@@ -77,17 +112,17 @@ public class Resolver {
 			}
 		}
 		
-    	String resultallenstring = resultallen.get(0);
-    	for( int i = 1; i < resultallen.size(); ++i ) {
-    		resultallenstring += "," + resultallen.get(i);
-    	}
-		
-		if( resultallen.isEmpty() ) {
+		// output the proofs result using the UserInterface
+    	if( resultallen.isEmpty() ) {
 			System.out.println("Fehler: Schnittmenge der Bedingung " + toProof.toString() + " ist leer und damit die Kante nicht konsistent");
 		} else {
+			String resultallenstring = resultallen.get(0);
+	    	for( int i = 1; i < resultallen.size(); ++i ) {
+	    		resultallenstring += "," + resultallen.get(i);
+	    	}
 			System.out.println("Die Schnittmenge von Bedingung " + toProof.toString() + " ist " + resultallenstring);
 		}
 		
-		return !resultallen.isEmpty();		
+		return !resultallen.isEmpty();	//return if there is an intersection
 	}
 }
